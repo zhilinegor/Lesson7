9de9a255629e914bd2caa5c668e000b02b5a3ac7
Егор Жилин <70974632+zhilinegor@users.noreply.github.com>
2022-12-16 15:56:44 +0300

    Add scripts

---
+++ array_nump.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr = np.random.randint(0,99, (3,3))
+print("\nМатрица:\n\n", arr)
+print("\nТранспонированная матрица:\n\n",arr.transpose())
---
+++ os.py
@@ -0,0 +1,5 @@
+import os
+
+print("Имя ОС:",os.name)
+print("Имя пользователя:",os.getlogin())
+print("Список файлов и директорий в папке:",os.listdir())
